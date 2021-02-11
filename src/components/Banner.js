ee468891098e069496a54391d16247083ad3993a
niemet0502 <vincentmarius8@gmail.com>
2021-02-11 16:54:57 +0100

    main :: added Banner content

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../style/Banner.css"
+function Banner() {
+  return (
+    <div className='lmj-banner'>
+      <h1>La maison jungle</h1>
+    </div>
+  )
+}
+
+export default Banner
