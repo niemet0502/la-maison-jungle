e6a1a70152ff6dd5c4cd72c22c0d687465540901
niemet0502 <vincentmarius8@gmail.com>
2021-02-12 17:22:38 +0100

    main :: imported and displayed ShoppingList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import Banner from "../components/Banner"
+import ShoppingList from "../components/ShoppingList"
 function App() {
   return (
     <div className="App">
       <Banner />
+      <ShoppingList />
     </div>
   );
 }
