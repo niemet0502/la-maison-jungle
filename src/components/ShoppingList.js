532a266c151601dc78c4055f02db1bd4172e40ca
niemet0502 <vincentmarius8@gmail.com>
2021-02-12 16:22:59 +0100

    main :: added Plantlist

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 function ShoppingList() {
+
+  const plantList = [
+    'monstera',
+    'ficus lyrata',
+    'pothos argenté',
+    'yucca',
+    'palmier'
+]
+
   return (
     <div>
       
